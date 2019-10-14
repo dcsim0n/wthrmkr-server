6b9586112de11d2b36aa32a0155170254aaf97ac
Dana Simmons <dsanalog@gmail.com>
2019-10-13 20:18:09 -0400

    Run server over https

--- run.py
+++ run.py
@@ -5,4 +5,6 @@
 
 from app import app
 
-app.run( host="0.0.0.0", port=3000, debug=True )
+# SSL = adhoc generates a new ssl cert every time the app runs
+# should be replaced with a certificate when deployed
+app.run( host="0.0.0.0", port=3000, debug=True, ssl_context="adhoc" )
