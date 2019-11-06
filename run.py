0ec63fff78ea1c463cdc161d1c8ce12cc71d57aa
Dana Simmons <dana@simmons-audio.com>
2019-11-05 19:20:13 -0500

    Temporarily turn off ssl
    using Ngrok to test with, which provides it's own ssl certs

--- run.py
+++ run.py
@@ -7,4 +7,5 @@ from app import app
 
 # SSL = adhoc generates a new ssl cert every time the app runs
 # should be replaced with a certificate when deployed
-app.run( host="0.0.0.0", port=3000, debug=True, ssl_context="adhoc" )
+#app.run( host="0.0.0.0", port=3000, debug=True, ssl_context="adhoc" )
+app.run(port=3000, debug=True)
